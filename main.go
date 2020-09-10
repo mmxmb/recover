4b87143e6a6538fa6f4b5835a37b53fa0d1f37ed
mmxmb <22385215+mmxmb@users.noreply.github.com>
2020-09-10 16:46:46 -0400

    Implement RecoverMiddleware that wraps existing Mux
    RecoverMiddleware adds panic stacktrace logging to existing web server and returns generic StatusInternalServerError if the server process panics.

--- main.go
+++ main.go
@@ -4,14 +4,38 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"runtime/debug"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/panic/", panicDemo)
-	mux.HandleFunc("/panic-after/", panicAfterDemo)
-	mux.HandleFunc("/", hello)
-	log.Fatal(http.ListenAndServe(":3000", mux))
+	rm := NewRecoverMiddleware()
+	log.Fatal(http.ListenAndServe(":3000", rm))
+}
+
+type RecoverMiddleware struct {
+	mux *http.ServeMux
+}
+
+func NewRecoverMiddleware() *RecoverMiddleware {
+	rm := &RecoverMiddleware{}
+	rm.mux = http.NewServeMux()
+	rm.mux.HandleFunc("/panic/", panicDemo)
+	rm.mux.HandleFunc("/panic-after/", panicAfterDemo)
+	rm.mux.HandleFunc("/", hello)
+	return rm
+}
+
+func (rm *RecoverMiddleware) recover(w http.ResponseWriter) {
+	if r := recover(); r != nil {
+		log.Printf("panic: %s\nstacktrace: %s\n", r, string(debug.Stack()))
+	}
+	http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+}
+
+func (rm *RecoverMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	defer rm.recover(w)
+	h, _ := rm.mux.Handler(r)
+	h.ServeHTTP(w, r)
 }
 
 func panicDemo(w http.ResponseWriter, r *http.Request) {
