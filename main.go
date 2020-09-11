2bb594e6122d1c1803c1c78c9d80cf25d54b26e7
mmxmb <22385215+mmxmb@users.noreply.github.com>
2020-09-10 22:19:40 -0400

    Implement RecoveryResponseWriter wrapper

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"runtime/debug"
 )
@@ -18,6 +20,7 @@ func main() {
 type Recovery struct {
 	mux *http.ServeMux
 	dev bool
+	rw  *RecoveryResponseWriter
 }
 
 func NewRecoverMiddleware(dev bool) *Recovery {
@@ -26,6 +29,7 @@ func NewRecoverMiddleware(dev bool) *Recovery {
 	rm.mux.HandleFunc("/panic/", panicDemo)
 	rm.mux.HandleFunc("/panic-after/", panicAfterDemo)
 	rm.mux.HandleFunc("/", hello)
+	rm.rw = &RecoveryResponseWriter{}
 	return rm
 }
 
@@ -44,7 +48,54 @@ func (rec *Recovery) recover(w http.ResponseWriter) {
 func (rec *Recovery) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer rec.recover(w)
 	h, _ := rec.mux.Handler(r)
-	h.ServeHTTP(w, r)
+	rec.rw.ResponseWriter = w
+	h.ServeHTTP(rec.rw, r)
+	rec.rw.flush()
+}
+
+type RecoveryResponseWriter struct {
+	http.ResponseWriter
+	writes [][]byte
+	status int
+}
+
+func (rw *RecoveryResponseWriter) Write(b []byte) (int, error) {
+	rw.writes = append(rw.writes, b)
+	return len(b), nil
+}
+
+func (rw *RecoveryResponseWriter) WriteHeader(statusCode int) {
+	rw.status = statusCode
+}
+
+func (rw *RecoveryResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	hijacker, ok := rw.ResponseWriter.(http.Hijacker)
+	if !ok {
+		return nil, nil, fmt.Errorf("the ResponseWriter does not support the Hijacker interface")
+	}
+	return hijacker.Hijack()
+}
+
+func (rw *RecoveryResponseWriter) Flush() {
+	flusher, ok := rw.ResponseWriter.(http.Flusher)
+	if !ok {
+		return
+	}
+	flusher.Flush()
+}
+
+func (rw *RecoveryResponseWriter) flush() error {
+	if rw.status != 0 {
+		rw.ResponseWriter.WriteHeader(rw.status)
+	}
+	for _, write := range rw.writes {
+		_, err := rw.ResponseWriter.Write(write)
+		if err != nil {
+			return err
+		}
+	}
+	rw.writes = nil
+	return nil
 }
 
 func panicDemo(w http.ResponseWriter, r *http.Request) {
